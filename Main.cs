776712258dae47c78ee382a9cd47bbad2883c240
Jonah <jonahcole@yahoo.com>
2022-11-07 15:38:05 -0800

    Add prompts and error handling

--- Main.cs
+++ Main.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 
 
-
-
-
 Connect4Game game = new Connect4Game();
 
+Console.WriteLine(game.BoardToString());
 
 
 bool gameOver = false;
@@ -15,77 +13,57 @@ while (gameOver == false)
 {
     Console.WriteLine($"Player-{game.currentPlayer} please enter column number:");
 
-    input = Convert.ToInt32(Console.ReadLine());
-
-    if (game.CheckColumn(input-1))
+    bool correctInput = false;
+    while (correctInput == false) 
     {
-        game.MakePlay(input-1, game.currentPlayer);
-
-        Console.WriteLine(game.BoardToString());
-
-        if (game.CheckWin(game.currentPlayer))
+        try
         {
-            Console.WriteLine($"Player-{game.currentPlayer} wins!");
-            break;
+            input = Convert.ToInt32(Console.ReadLine());
+            if (input >= 1 && input <=7) 
+            {
+                if (game.CheckColumn(input-1))
+                {
+                    game.MakePlay(input-1, game.currentPlayer);
+
+                    Console.WriteLine(game.BoardToString());
+
+                    if (game.CheckWin(game.currentPlayer))
+                    {
+                        Console.WriteLine($"Player-{game.currentPlayer} wins!");
+                        gameOver = true;
+                        break;
+                    }
+
+                    if (game.CheckNumPlays() == false)
+                    {
+                        Console.WriteLine("Game has ended in a draw!");
+                        gameOver = true;
+                        break;
+                    }
+
+                    game.SwitchPlayer();
+                }
+                else 
+                {
+                    Console.WriteLine("That column is full!");
+                }
+                correctInput = true;
+            }
+            else
+            {
+                Console.WriteLine("Input must be between 1 and 7");
+                Console.WriteLine($"Player-{game.currentPlayer} please enter column number:");
+            }     
+        } 
+        catch (FormatException)
+        {
+        Console.WriteLine("Input must be a number");
+        Console.WriteLine($"Player-{game.currentPlayer} please enter column number:");
         }
 
-        if (game.CheckNumPlays() == false)
-        {
-            Console.WriteLine("Game has ended in a draw!");
-            break;
-        }
-
-        game.SwitchPlayer();
-    }
-    else 
-    {
-        Console.WriteLine("That column is full!");
     }
 
     
 
 }
-
-
-
-
-
-
-
-// Console.WriteLine("Please choose an option\n1. Start game \n2. Exit");
-
-// //Input verification (gotta be cleaner way to do it)
-// //will only accept 1 or 2 
-// bool correctInput = false;
-// while (correctInput == false) 
-// {
-//     try
-//     {
-//         var input = Convert.ToInt32(Console.ReadLine());
-//         if (input == 1) 
-//         {
-//             Console.WriteLine("Starting game...");
-//             correctInput = true;
-//         } 
-//         else if (input == 2) 
-//         {
-//             Console.WriteLine("Exiting...");
-//             correctInput = true;
-//         }
-//         else 
-//         {
-//             Console.WriteLine("Input must be 1 or 2");
-//         }       
-//     } 
-//     catch (FormatException)
-//     {
-//     Console.WriteLine("Input must be a number");
-//     }
-
-// }
-
-
-
-
-
 
